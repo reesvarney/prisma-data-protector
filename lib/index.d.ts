ca2301c67760c0b25e9f605a2b51f531630a7fbf
Rees Varney <reesvarney02@gmail.com>
2022-08-25 19:11:50 +0100

    build for last commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,9 @@
 /**
- *
- * @param models (Prisma.dmmf.datamodel.models) - Provides the fields to be included
- * @param data Dictionary of fields to filter out for each model
+ * Prisma middleware to filter out certain fields when a SELECT parameter is not specified on a query
+ * @param {Object} models (Prisma.dmmf.datamodel.models) - Provides the fields to be included
+ * @param {Object[]} data Dictionary of fields to filter out for each model
+ * @param {Object} options Options for the middleware
+ * @param {Boolean} options.warn Whether to log a warning when a query is filtered, default is true
  * @returns Prisma Middleware
  */
 export declare function protectData(models: any, data: {
